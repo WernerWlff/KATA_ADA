2528e3cd899d7248d8e66bc3144462fb0aa52929
Werner <werner.wolff1109@gmail.com>
2025-07-04 11:14:53 +0200

    Ajout étape 5 / ajout commentaires

--- quotations-app/script.js
+++ quotations-app/script.js
@@ -3,7 +3,8 @@ const NOM_AUTEUR = document.getElementById("nomAuteur")
 const AJOUT_BUTTON = document.getElementById("ajoutButton")
 const TEXTE_CITATIONS = document.getElementsByClassName("text")
 const AUTHOR_CITATIONS = document.getElementsByClassName("author")
-
+const QUOTE = document.getElementsByClassName("quote")
+const QUOTE_LIST = document.getElementById("quote-list")
 /*--------------------------------------------------------------------------------------------------------------------------------------------*/
 
 AJOUT_BUTTON.addEventListener("click", ajoutCitation)
@@ -14,7 +15,32 @@ AJOUT_BUTTON.addEventListener("click", ajoutCitation)
 function ajoutCitation(event){
     let citation = CITATION.value
     let nomAuteur = NOM_AUTEUR.value
-    console.log(citation)
-    console.log(nomAuteur)
+    // console.log(citation)
+    // console.log(nomAuteur)
+    
     event.preventDefault() // Permet d'empecher le rechargement de notre page a chaque fois qu'on appuie sur le bouton
+
+    //on va appeler notre fonction d'apres pour rajouter nos données dans un paragraphe cf.étape 5  
+    addQuote(citation, nomAuteur)
+}
+
+//on va vraisemblablement l'appeler à la fin de ajoutCitation car c'est la deuxième étape
+function addQuote(quote, author){
+    const NOUVELLE_CITATION = document.createElement('p')
+    const NOUVEL_AUTEUR = document.createElement('p')
+    NOUVELLE_CITATION.classList.add("text")
+    NOUVEL_AUTEUR.classList.add("author")
+    NOUVELLE_CITATION.innerText = quote
+    NOUVEL_AUTEUR.innerText = author
+
+    const NOUVELLE_DIV = document.createElement('div')
+    NOUVELLE_DIV.classList.add("quote")
+
+    //On ajoute nos deux 'p' dans la nouvelle balise 'div'
+    NOUVELLE_DIV.appendChild(NOUVELLE_CITATION)
+    NOUVELLE_DIV.appendChild(NOUVEL_AUTEUR)
+
+    //On ajoute notre nouvelle balise 'div' dans la 'div' quote-list
+    QUOTE_LIST.appendChild(NOUVELLE_DIV)
+
 }
