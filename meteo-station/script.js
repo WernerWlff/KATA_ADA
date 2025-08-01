e31408cf0e042f300068286b562177ebb92d7f35
Werner Wolff <werner.wolff1109@gmail.com>
2025-08-01 10:49:19 +0200

    chore : MVP

--- meteo-station/script.js
+++ meteo-station/script.js
@@ -1,1 +1,11 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+const city = document.getElementById('city')
+const gps = document.getElementById('gps')
+const temperature = document.getElementById('temperature')
+const details = document.getElementById('details')
+const cityButton = document.getElementById('cityButton')
+const cityInput = document.getElementById('cityInput')
+
+async function getWeatherCity(cityName){
+    const response = fetch('')
+    
+}
