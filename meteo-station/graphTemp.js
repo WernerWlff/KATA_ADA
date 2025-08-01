26f7ea00795ab69e3ee63f64c94898926db17bbb
Werner Wolff <werner.wolff1109@gmail.com>
2025-08-01 13:00:22 +0200

    Finished Project

--- meteo-station/graphTemp.js
+++ meteo-station/graphTemp.js
@@ -2,7 +2,8 @@ const cityButton = document.getElementById('cityButton')
 const graphTemperature = document.getElementById('graphTemperature')
 let temperatureGraph
 
-cityButton.addEventListener('click', function () {
+
+document.addEventListener('DOMContentLoaded', function () {
 const hoursLabels = ['heure1','heure2','heure3','heure4','heure5','heure6','heure7','heure8','heure9','heure10','heure11','heure12','heure13','heure14','heure15',]
 const data = {
   labels: hoursLabels,
@@ -36,8 +37,8 @@ const config = {
 
 export async function updateChartWithData(latGiven, LonGiven){
 
-    const graphData = new Array(15).fill(0)
-    const graphHours = new Array(15).fill('')
+    let graphData = new Array(15).fill(0)
+    let graphHours = new Array(15)
     
     try{
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latGiven}&longitude=${LonGiven}&hourly=temperature_2m,precipitation&past_days=2`)
@@ -47,8 +48,11 @@ export async function updateChartWithData(latGiven, LonGiven){
         graphData[i] = answer.hourly.temperature_2m[i + 13]
         graphHours[i] = answer.hourly.time[i + 13]
     }
-        temperatureGraph.data.datasets[0].data = graphData
-        hoursLabels = graphHours
+
+
+    temperatureGraph.data.datasets[0].data = graphData
+
+    temperatureGraph.data.labels = graphHours
     
     temperatureGraph.update()
     }
